54711113e47b3967f63c4ba794d1dc3f66a2921e
Jacob Kaplan <jtkaplan@wpi.edu>
2019-02-26 06:09:30 -0500

    continued work

--- src/PollutionCloudMap.js
+++ src/PollutionCloudMap.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson';
 
 var us  = require('us-atlas/us/10m.json');
-
+var state_names = require("./data/state_names.js").default;
 
 
 class PollutionCloudMap extends Component {
@@ -33,6 +33,8 @@ class PollutionCloudMap extends Component {
 			.attr("offset","0")
 			.style("stop-color", "#464547")
 
+		console.log(this.radgrad)
+
 		this.upperbounds = this.radgrad.append("stop")
 			.attr("offset","1")
 			.style("stop-color", "#464547")
@@ -41,21 +43,33 @@ class PollutionCloudMap extends Component {
 		this.defs.append("filter")
 			.attr('id',"goo")
 			.html(`<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-      				<feColorMatrix in="blur" mode="matrix" values= "1 0 0 0 0 
-      																0 1 0 0 0 
-      																0 0 1 0 0 
-      																0 0 0 18 -7" result="goo" />
-      				<feBlend in="SourceGraphic" in2="goo" />`)
+      <feColorMatrix in="blur" mode="matrix" values=
+                     "1 0 0 0 0
+                      0 1 0 0 0
+                      0 0 1 0 0
+                      0 0 0 36 -7" result="almostdone" />
+      <feGaussianBlur in="almostdone" stdDeviation="5" result="goo" />
+      <feBlend in="SourceGraphic" in2="goo" />`)
 
-		console.log(this.defs.nodes())
+	}
 
+	makeCustomSmokes(){
+		var classes = us.objects.states.geometries.map(d=>d.id)
 
+		classes.forEach(d=>{
+		this.defs.append("radialGradient")
+			.attr("id", d)
+			.attr("cx", "50%")
+			.attr("cy", "50%")
+			.attr("r", "50%")
+			.html(`<stop class="start" offset="0" stop-color="#464547" stop-opacity="0.2"></stop><stop class="end" offset="1" stop-color="#464547" stop-opacity="0"></stop>`)
+		})
 	}
 
 	componentDidMount(){
 
 		this.init();
-
+		this.makeCustomSmokes()
 
 
 		var path = d3.geoPath();
@@ -74,12 +88,6 @@ class PollutionCloudMap extends Component {
 
 		this.mapArea = this.svg.append("g")
 
-		var states = this.mapArea.append("g")
-			.attr("class", "states")
-			.selectAll("path")
-			.data(us.objects.states.geometries)
-			.enter()
-
 		var smokes = this.mapArea.append("g")
 			.style('filter', "url(#goo)")
 			.attr("class", "smokes")
@@ -87,42 +95,91 @@ class PollutionCloudMap extends Component {
 			.data(us.objects.states.geometries)
 			.enter()
 
+		var stateNames = this.mapArea.append("g")
+
+		var states = this.mapArea.append("g")
+			.attr("class", "states")
+			.selectAll("path")
+			.data(us.objects.states.geometries)
+			.enter()
+
+
+
 		states.append("path")
 			.attr("stroke-width", 0.5)
 			.attr("stroke", "blue")
 			.attr("fill", "None")
 			.attr("d", d=>path(topojson.feature(us, d, function(a, b) { return a !== b; })))
+			.each(function(d){
+				let bbox = this.getBBox();
+				let cx = bbox.width / 2 + bbox.x;
+				let cy = bbox.height / 2 + bbox.y;
+				stateNames.append("text")
+					.attr("class", d.id)
+					.attr("x", cx)
+					.attr("y", cy)
+					.attr("text-anchor", "middle")
+					.text(state_names[d.id].STATE_NAME)
+			})
 
 		smokes//.append("g")
 			//.attr("transform", "scale(1.5)")
 			.append("path")
 			.attr("d", d=>path(topojson.feature(us, d, function(a, b) { return a !== b; })))
-			.attr("fill", "url(#smoke)")
+			.attr("class", d=>d.id)
+			.attr("fill", d=>"url(#"+d.id+")")
+			/*
 			.each(function(d){
 				let bbox = this.getBBox();
 				let cx = bbox.width / 2 + bbox.x;
 				let cy = bbox.height / 2 + bbox.y;
 				let scale = 1.1;
-				console.log(bbox);
 
-				d3.select(this)
+				stateNames.append("text")
+					.attr("x", cx)
+					.attr("y", cy)
+					.text(state_names[d.id].STATE_NAME)
+				//console.log(bbox);
+
 					//.attr("transform", "translate("+ cx +","+ cy +") scale("+scale+") translate("+ -cx +","+ -cy +")");
-			})
-			.style("fill-opacity", 0.5);
+			})*/
+			.on("click", d=>{console.log(state_names[d.id].STATE_NAME)})
+			//.style("fill-opacity", 0.5);
 
 		this.svg.on("click", (d)=>{
 				console.log(this.lowerbounds)
+				console.log(this.defs)
+				var anim = () => {
+					var newval;
+					this.defs.selectAll("stop.start")
+						.transition()
+						.attr("offset", function(d){
+							newval = Math.random();
+							stateNames.select("[class='"+d3.select(this).node().parentNode.id+"']")
+								.text(newval.toFixed(3))
+							return newval**2})
+						.attr("stop-opacity", d=>Math.random())
+						.duration(500)
+/*						.transition()
+						.attr("offset", 0)
+						.attr("stop-opacity", 0.2)
+						.duration(500)*/
+						.each(function(d){
+							//console.log(d3.select(this).node().parentNode.id)
+							//stateNames.select("[class='"+d3.select(this).node().parentNode.id+"']")
+							//	.text(newval.toFixed(3))
+						})
+						.on("end", anim);
+				}
 
-				if(this.lowerbounds.attr("offset") == 0){
-					this.lowerbounds.transition()
-						.attr("offset", 1)
-						.duration(1500)
+				anim()
 
-				} else{
+/*
 					this.lowerbounds.transition()
 						.attr("offset", 0)
+						.attr("stop-opacity", 0)
 						.duration(300)
-				}
+*/				
 
 
 
