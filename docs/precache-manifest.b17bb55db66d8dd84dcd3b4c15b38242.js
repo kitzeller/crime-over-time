eeb0dd0ad64537778ce08ddb4242b71edd71c076
Jacob Kaplan <jtkaplan@wpi.edu>
2019-02-28 08:52:01 -0500

    asd

---
+++ docs/precache-manifest.b17bb55db66d8dd84dcd3b4c15b38242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d88043b94138f05545f0",
+    "url": "/crime-over-time/static/js/runtime~main.d88043b9.js"
+  },
+  {
+    "revision": "7dabbc6c58c25160a909",
+    "url": "/crime-over-time/static/js/main.7dabbc6c.chunk.js"
+  },
+  {
+    "revision": "878ac71c55db00d65041",
+    "url": "/crime-over-time/static/js/2.878ac71c.chunk.js"
+  },
+  {
+    "revision": "7dabbc6c58c25160a909",
+    "url": "/crime-over-time/static/css/main.3050c53e.chunk.css"
+  },
+  {
+    "revision": "6aba8b5099551a5f60e7c7321727bd3a",
+    "url": "/crime-over-time/index.html"
+  }
+];
