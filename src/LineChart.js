32ced955947d6a21fd54c88b7e7cdfd261bda0f2
kitzeller <kit.zeller@gmail.com>
2019-02-28 08:34:35 -0500

    More Axis Updates

--- src/LineChart.js
+++ src/LineChart.js
@@ -255,7 +255,7 @@ class LineChart extends Component {
             .attr("dy", "1em")
             .style("text-anchor", "middle")
             .style("fill","red")
-            .text("Crime");
+            .text("Crime Rate (per 100,000 people)");
 
         this.svg.append("text")
             .attr("transform", "rotate(-90)")
@@ -264,7 +264,7 @@ class LineChart extends Component {
             .style("fill","grey")
             .attr("dy", "1em")
             .style("text-anchor", "middle")
-            .text("Lead");
+            .text("Lead (grams purchased as Gasoline)");
     };
 
     render() {
